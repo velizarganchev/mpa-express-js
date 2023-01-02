21391ce2610d28a929e013e0b3de3faa9f3bf11a
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-01-02 18:57:53 +0100

    add start expres app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("server started");
+const express = require('express');
+
+const app = express();
+
+app.all('/', (req, res) => {
+    res.write('It is working');
+    res.end()
+});
+app.listen(5000, console.log.bind(console, 'app is running on http://localhost:5000'));
